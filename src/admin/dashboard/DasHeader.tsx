3c6b58291e46f2799e419af173c493a31371a2aa
Dhanielgeek <dhanielknightz46@gmail.com>
2025-02-06 19:47:47 +0100

    Finished the admin blog part...

---
+++ src/admin/dashboard/DasHeader.tsx
@@ -0,0 +1,10 @@
+const DasHeader = () => {
+  return (
+    <header className="bg-white p-4 shadow-md flex justify-between">
+      <h1 className="text-lg font-semibold">Blog Platform</h1>
+      <button className="text-red-500">Logout</button>
+    </header>
+  );
+};
+
+export default DasHeader;
